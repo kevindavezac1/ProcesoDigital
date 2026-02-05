40c7a9dda6435b109cf0bc3514fce33d9ee4937a
User <user@Users-MacBook-Air.local>
2026-02-05 22:56:40 +0000

    arregle los textos

--- script.js
+++ script.js
@@ -36,3 +36,18 @@ steps.forEach(step=>{
   step.style.transition = "all .8s cubic-bezier(.2,.8,.2,1)";
   observer.observe(step);
 });
+
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
